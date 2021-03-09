57e7fb53e7e8a2def8692442ccbf66f11e269be2
Denys Symak <dreamwinamounts@gmail.com>
2021-03-09 15:34:57 +0200

    @1.1.2

--- src/assets/server.php
+++ src/assets/server.php
@@ -1,2 +1,3 @@
 <?php
+$_POST = json_decode(file_POST_contents("php://input"), true);
 echo var_dump($_POST);
--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,16 @@
+'use strict';
+
 import './slider';
 
 import {
     modals,
-    tabs
+    tabs,
+    forms
 } from './modules/';
 
 window.addEventListener('DOMContentLoaded', () => {
     modals();
     tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
     tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    forms();
 });
-// modals();
